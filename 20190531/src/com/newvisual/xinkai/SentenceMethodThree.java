0cf6b0366a3a699bd5e4fd71e3fab2e755c09a15
minxin wang <359676107@qq.com>
2019-06-04 00:09:51 +0800

    递归

--- 20190531/src/com/newvisual/xinkai/SentenceMethodThree.java
+++ 20190531/src/com/newvisual/xinkai/SentenceMethodThree.java
@@ -6,5 +6,25 @@ package com.newvisual.xinkai;
  * @Date:2019-6-2
  */
 public class SentenceMethodThree {
+    private static void sum(int a, int b) {
+        int i;
+        int sum = 0;
+        for (i = a; i <= b; i++) {
+            sum += i;
+        }
+        System.out.println(a + "到" + b + "的和是" + sum);
+    }
 
+    private static int factor(int i) {
+        if (i == 1) {
+            return i;
+        }
+        return i * factor(i - 1);
+    }
+
+    public static void main(String[] args) {
+        int k = factor(3);
+        sum(1, 2);
+        System.out.println(k);
+    }
 }
--- 20190531/src/com/newvisual/xinkai/SentenceMethodTwo.java
+++ 20190531/src/com/newvisual/xinkai/SentenceMethodTwo.java
@@ -23,7 +23,6 @@ public class SentenceMethodTwo {
                 break;
             }
         }
-
     }
 
     /**
