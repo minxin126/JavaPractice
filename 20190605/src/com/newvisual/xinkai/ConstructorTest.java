9a89815459c1334109916a562aa8ff41c902cb2b
minxin wang <359676107@qq.com>
2019-06-05 01:04:22 +0800

    函数重载

---
+++ 20190605/src/com/newvisual/xinkai/ConstructorTest.java
@@ -0,0 +1,65 @@
+package com.newvisual.xinkai;
+
+import java.util.Random;
+
+/**
+ * @Descrption: 函数重载，参数不同
+ * @Author: minx
+ * @Date: 2019-6-5
+ */
+public class ConstructorTest {
+
+    public static void main(String[] args) {
+        Employee[] staff = new Employee[3];
+        staff[0] = new Employee("harry", 40000);
+        staff[1] = new Employee(60000);
+        staff[2] = new Employee();
+        for (Employee e : staff) {
+            System.out.println("name:" + e.getName() + ",id:" + e.getId() + ",salary=" + e.getSalary());
+        }
+    }
+
+}
+
+class Employee {
+    private static int nextId;
+    private int id;
+    private String name = "";
+    private double salary;
+
+    static {
+        Random generator = new Random();
+        nextId = generator.nextInt(10000);
+    }
+
+    {
+        id = nextId;
+        nextId++;
+    }
+
+    Employee(String n, double s) {
+        name = n;
+        salary = s;
+    }
+
+    Employee(double s) {
+        this("Employee #" + nextId, s);
+    }
+
+    Employee() {
+
+    }
+
+    String getName() {
+        return name;
+    }
+
+    double getSalary() {
+        return salary;
+    }
+
+    int getId() {
+        return id;
+    }
+
+}
