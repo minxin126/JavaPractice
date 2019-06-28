8639de0b541ba97b96d7d655650fc856415e681f
minxin wang <359676107@qq.com>
2019-06-29 00:33:06 +0800

    解释了hashset和hashmap的区别，set为集合，map与主键关联的集合。

--- notebook/src/coins/Coin.java
+++ notebook/src/coins/Coin.java
@@ -14,12 +14,16 @@ public class Coin {
      */
     private HashMap<Integer, String> coinnames = new HashMap<Integer, String>();
 
+    /**
+     * 我知道了，set只是一个集合，而hashmap为主键的集合
+     */
     public Coin() {
         coinnames.put(1, "penny");
         coinnames.put(10, "dime");
         coinnames.put(25, "quarter");
         coinnames.put(50, "half-dolar");
         coinnames.put(50, "五毛");
+        //keySet()为一个集合
         System.out.println(coinnames.keySet().size());
         System.out.println(coinnames);
         for (Integer k : coinnames.keySet()) {
